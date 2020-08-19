a3a5d02beaca62c78af498820a61576178f0f6e9
Jason <39965054+jweng1@users.noreply.github.com>
2020-08-19 16:18:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    README = readme_file.read()
+# with open('README.md') as readme_file:
+#     README = readme_file.read()
 
-with open('HISTORY.md') as history_file:
-    HISTORY = history_file.read()
+# with open('HISTORY.md') as history_file:
+#     HISTORY = history_file.read()
 
 setup_args = dict(
     name='gstreamer-rgw-sink',
